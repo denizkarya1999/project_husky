9102cd9a6f24cd6765d9f5530604e7719faa748a
Deniz Karya Acikbas <denizkarya1999@outlook.com>
2021-06-03 19:05:22 -0400

    First Commitment

---
+++ project_husky/project_husky/currency_convertor.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace project_husky
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class currency_convertor : Page
+    {
+        public currency_convertor()
+        {
+            this.InitializeComponent();
+        }
+
+        private void convert_from_textbox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            convert_from_textbox.Text = " ";
+        }
+
+    }
+}
